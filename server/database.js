0327fa7d592b5c0865e7bcd84980b1129b251dc4
AVB <antonybyju4444@email.com>
2024-05-14 23:21:34 +0530

    Server-side signUp, login, token regenerartion, logout etc., have been created. Changes have been made to server1.js and database,js

--- server/database.js
+++ server/database.js
@@ -11,39 +11,89 @@ const pool = mysql.createPool({
     database: process.env.DATABASE
 }).promise()
 
-export async function setData(Fnam,Lnam,mail,pass){
+export async function setData(mail, pass, Fnam, Lnam){
 
-    let qry2 = "SELECT * FROM angularPrjt WHERE EMAIL=? AND PASSWORD=?"
-    let [num] = await pool.query(qry2,[mail,pass])
+    let qry2 = "SELECT * FROM angularPrjt WHERE EMAIL=?"
+    let [num] = await pool.query(qry2,[mail])
+    //console.log("Value in num: ", num[0])
+    //console.log("Value of lenght: ",num.length)
     if (num.length>0){
 
-        return -1
+        return 1
     }
     else{
-        
-        let qry = "INSERT INTO angularPrjt(EMAIL, PASSWORD, FNAME, LNAME) VALUES(?,?,?,?)"
+        try {
 
-
-        newPass = 
-
-
-        await pool.query(qry,[Fnam,Lnam,mail,pass])
-        let qry2 = "INSERT INTO tknHldr(EMAIL, TOKEN) VALUES(?, ?)"
-        await pool.query(qry2, [mail, null])
-        return 1
+            //let salt = await bcrypt.genSalt()
+            let newPass = await bcrypt.hash(pass, 10)
+            
+            //console.log("Salt and newPAss: ",salt, newPass)
+            let qry = "INSERT INTO angularPrjt(FNAME, LNAME, EMAIL, PASSWORD) VALUES(?,?,?,?)"
+            await pool.query(qry,[Fnam,Lnam,mail,newPass])
+            //console.log("Data inserte")
+            let qry2 = "INSERT INTO tknHldr(EMAIL, TOKEN) VALUES(?, ?)"
+            await pool.query(qry2, [mail, null])
+    
+        }
+        catch{
+            //console.log("Error in inserting data")
+            return 2
+        }
+        return 3
     }
 }
 
 export async function loginCheck(mail,pass)
 {
-    let qry = "SELECT * FROM REGWEB WHERE EMAIL=? AND PASSWORD=?"
-    let [num] = await pool.query(qry,[mail,pass])
-    if (num.length>0){
-        let qry1 = "SELECT ID FROM REGWEB WHERE EMAIL=? AND PASSWORD=?"
-        let id = await pool.query(qry1,[mail,pass])
-        return id[0][0].ID
+    let qry = "SELECT PASSWORD FROM ANGULARPRJT WHERE EMAIL=?"
+    let [num] = await pool.query(qry,[mail])
+    console.log("Password from the DB: ", num[0].PASSWORD)
+    try{
+        if (await bcrypt.compare(pass, num[0].PASSWORD)){
+    
+            return 1
+            // let qry1 = "SELECT ID FROM REGWEB WHERE EMAIL=? AND PASSWORD=?"
+            // let id = await pool.query(qry1,[mail,pass])
+            // return id[0][0].ID
+        }
+        else{
+            return -1
+        }
+    }
+    catch{
+        return 0
+    }
+}
+
+export async function storeToken(mail, refTkn) {
+
+    let qy = "UPDATE TKNHLDR SET TOKEN=? WHERE EMAIL=?"
+    await pool.query(qy, [refTkn, mail], (err) =>{
+        if (err){
+            return -1
+        }
+    })
+
+    return 1
+}
+
+export async function ifTknValid(refTk){
+
+    let qre = "SELECT EMAIL FROM TKNHLDR WHERE TOKEN=?"
+    let [ml] = await pool.query(qre, [refTk])
+    console.log("Mail id: ", ml[0])
+    if (ml[0] != null){
+        return ml[0].EMAIL
     }
     else{
-        return -1
+        return null
     }
+}
+
+export async function dltToken(mail){
+    let qre = "UPDATE TKNHLDR SET TOKEN=? WHERE EMAIL=?"
+    await pool.query(qre, [null, mail], (err) => {
+        if (err) return -1
+    })
+    return 1
 }
--- server/server1.js
+++ server/server1.js
@@ -2,29 +2,41 @@ import express from 'express'
 import cors from 'cors'
 import jwt from 'jsonwebtoken'
 import dotnev from 'dotenv'
+import {setData, loginCheck, storeToken, ifTknValid, dltToken} from './database.js'
 
 dotnev.config()
 const app = express()
 app.use(express.json())
 app.use(cors())
 
-let refTokens = []
+//let refTokens = []
 
-app.post('/login',(req, res) => {
+app.post('/login', async (req, res) => {
 
     console.log(req.url)
 
-    let username = req.body.username
+    let username = req.body.email
     let password = req.body.password
     console.log(username,password)
 
-    if (loginCheck(username, password)) {
+    let retVal = await loginCheck(username, password)
+    console.log("Value returned by loginCheck(): ", retVal)
+    if (retVal == 1) {
 
         let token = generateToken(username)
         let refToken = jwt.sign({ user: username }, process.env.REFRESH_TOKEN)
-        refTokens.push(refToken)
-    
-        res.status(201).send({status: true, reqToken: token, refreshToken: refToken})
+        //refTokens.push(refToken)
+        let resL= await storeToken(username, refToken)
+        if (resL == 1){
+            res.status(201).send({status: true, reqToken: token, refreshToken: refToken})            
+        }
+        else{
+            res.status(401).send({message: "Token storage error"})
+        }
+        //res.status(201).send({"message": "Sign in Successful"})
+    }
+    else if (retVal == -1){
+        res.status(400).send({status: false, message: "Inavalid username or password"})
     }
     else {
         res.status().send({status: false, reqToken: "", refreshToken: ""})
@@ -33,33 +45,90 @@ app.post('/login',(req, res) => {
 
 })
 
-app.post('/token', (req, res) => {
+app.post('/signUp', async (req, res) => {
+    //console.log("Requested URL: ",req.url)
 
-    let refTok = req.body.token
-    if (refTok == null) return res.sendStatus(401)
-    if (!refTokens.includes(refTok)) return res.sendStatus(403)
+    let email = req.body.email
+    let pass = req.body.password
+    let fname = req.body.fname
+    let lname = req.body.lname
 
-    jwt.verify(refTok, process.env.REFRESH_TOKEN, (err, user) => {
-    if (err) return res.sendStatus(403)
-    let newReqTok = generateToken(user.user)
-    res.send({reqToken: newReqTok})
-
-    })
+    let rslt = await setData(email, pass, fname, lname)
+    //console.log("Value returend by function: ",rslt)
+    if (rslt == 3){
+        res.status(201).send({message: "Sign up Successfull"})
+    }
+    else if (rslt == 2){
+        res.status(401).send({message: "Error during insertion"})
+        
+    }
+    else{
+        res.status(400).send({message: "User already exists"})
+    }
 })
 
-app.post('/logout', (req, res) => {
+app.post('/token', async (req, res) => {
+
+    let refTok = req.body.token
+    if (refTok == null) return res.status(401).send({message: "No valid token"})
+    // if (!refTokens.includes(refTok)) return res.sendStatus(403)
+    let vl = await ifTknValid(refTok)
+    console.log("Value returned by ifTknValid(): ", vl)
+    if (vl != null){
+
+        jwt.verify(refTok, process.env.REFRESH_TOKEN, (err, user) => {
+        if (err) return res.status(403).send({message: "Verification of token failed"})
+        if (user.user == vl){ 
+            let newReqTok = generateToken(user.user)
+            res.send({message: "New access token",reqToken: newReqTok})
+        }
+        else{
+            res.status(401).send({message: "User of refresh token not matching"})
+        }
+    
+        })
+    }
+    else{
+        res.status(401).send({message: "Token not matching the user"})
+    }
+
+})
+
+app.post('/logout', async (req, res) => {
 
     let refTo = req.body.token
 
-    if (!refTokens.includes(refTo)) return res.sendStatus(403)
-    refTokens.pop(refTo)
+    // if (!refTokens.includes(refTo)) return res.sendStatus(403)
+    // refTokens.pop(refTo)
+    let nvl = await ifTknValid(refTo)
 
-    res.send({message: "Sucess"}).status(200)
+    if (nvl != null){
+        jwt.verify(refTo, process.env.REFRESH_TOKEN, async (err, user) => {
+            if (err) return res.status(401).send({message: "Verification error"})
+            console.log("Value in /logout from ifValidTkn(): ", nvl)
+            console.log("Value of user in /logout from verify: ", user)
+
+            if (nvl == user.user){
+                let nlv = await dltToken(user.user)
+                if (nlv == 1){
+                    res.send({message: "Logout Sucess"}).status(200)
+                }
+                else{
+                    res.send({message: "Logout unsuccessfull"}).status(401)
+                }
+            }
+            else{
+                res.status(400).send({message: "Invalid Refresh Token"})
+            }
+        })
+    }
+
 })
 
 function generateToken(user) {
     return jwt.sign({ user: user }, process.env.SECRET_ACCESS_TOKEN, {expiresIn: '20s'})
 }
+
 
 app.listen(3000, (err) => {
     
