25763984bbbd5f74aadc331953f335ed233bbe42
AVB <antonybyju4444@email.com>
2024-05-16 08:38:16 +0530

    Removed all commented code from auth.service.ts

--- frontEnd/src/app/services/auth.service.ts
+++ frontEnd/src/app/services/auth.service.ts
@@ -11,64 +11,15 @@ export class AuthService {
 
   constructor(private router: Router, private http: HttpClient) {}
 
-  tokenHandling(){
-    if (!this.isTokenExpired()){
-      this.refToken()
-      return localStorage.getItem("reqTkn") 
-    }
-    else{
-      return localStorage.getItem("reqTkn")
-    }
-
-  }
-
-  // getData() {
-    
-    // let retVal = this.isTokenExpired()
-    // console.log("Is token valid: ",retVal)
-    
-    // if (!retVal){
-    //   this.refToken()
-    // }
-    
-    // let jwttoken = localStorage.getItem("reqTkn")
-    //this.tokenHandling()
-    
-    // if (!this.isTokenExpired()){
-    //   this.refToken().subscribe((res: any) => {
-    //     console.log("Result of refToken(): ", res)
-    //     console.log("Sending the get request")
-    //     return this.http.get("http://localhost:4000/admin/home") //No need to add the token to the header as the interceptor will do it
-    //   })
-    // }
-    // return this.http.get("http://localhost:4000/admin/home")
-    // {
-    //   headers: {
-    //     authorization: 'Bearer ' + jwttoken,
-    //   },
-    // })
-  //}
-
-  // async getData() {
-    
-  //   let jwttoken = await this.tokenHandling()
-  //   console.log(jwttoken)
-  //   console.log("Sending the get request")
-  //   return this.http.get("http://localhost:4000/admin/home", {
-  //     headers: {
-  //       authorization: 'Bearer ' + jwttoken,
-  //     },
-  //   })
-  // }
-
+  
   setToken(reqTkn: string, token: string){
     localStorage.setItem(reqTkn, token)
   }
-
+  
   getToken(itmName: string){
     return localStorage.getItem(itmName)
   }
-
+  
   loggedIn(){
     let tknVl = this.getToken("refTkn")
     console.log("Value of token in loggedIn(): ", tknVl )
@@ -80,119 +31,94 @@ export class AuthService {
     }
     //return this.getToken("refTkn") != null
   }
-
+  
   logoutNow(){
-
+    
     let tkn = {
       token: localStorage.getItem("refTkn")
     }
-
+    
     this.http.post("http://localhost:3000/logout", tkn).subscribe((res: any) => {
       console.log(res)
     })
-
+    
     localStorage.removeItem('refTkn')
     this.router.navigate(['/login'])
   }
-
+  
   newData(data: any) {
     
     return this.http.post("http://localhost:3000/login",data).subscribe((res: any) => {
       if (res.status)
-      {
-        this.setToken("reqTkn", res.reqToken)
-        console.log("Request Token: ", this.getToken("reqTkn"))
-        this.setToken("refTkn", res.refreshToken)
-        console.log("Refresh Token: ", this.getToken("refTkn"))
-        this.router.navigate(['/admin/home'])
-      }
-    })
-  }
-
-  isTokenExpired() {
-    let tkn = localStorage.getItem("reqTkn")
-    if (!tkn) return true
-
-    let decode = jwtDecode(tkn)
-    if (!decode.exp) return true
-    let expiration_time = decode.exp * 1000
-    let timeNow = new Date().getTime()
-    
-    //console.log("Value of experation and current time: ", expiration_time, timeNow)
-
-    return expiration_time > timeNow
-  }
-
-  // refToken(){
-  //   let tkn = {
-  //     token: localStorage.getItem("refTkn")
-  //   }    
-  //   return this.http.post("http://localhost:3000/token", tkn).subscribe((res: any) => {
-  //     if (res.status){
-  //       console.log("Refreshed token: ", res)
-  //       this.setToken("reqTkn", res.reqToken)
-  //     }
-  //   })
-  // }
-
-  // refToken() {
-  //   let token1 = {
-  //     token: this.getToken("refreshTkn")
-  //   } 
-  //   return this.http.post("http://localhost:3000/token", token1).subscribe((res: any) => {
-  //     if (res.status){
-  //       console.log("Refreshed token: ", res)
-  //       this.setToken("reqTkn", res.reqToken)
-  //       return true
-  //     }
-  //     return false
-  //   })
-  // }
-
-  getData(): Observable<any> {
-
-    if (!this.isTokenExpired()) {
-
-      return this.refToken().pipe(
-        switchMap(() => {
-
-          console.log("Sending the get request after token refresh");
-          return this.http.get("http://localhost:4000/admin/home");
-        }),
-        catchError(error => {
-
-          console.error('Error refreshing token', error);
-          return of(null); // Handle error appropriately
-        })
-      );
-    } 
-    else {
-
-      console.log("Sending the get request without token refresh");
-      return this.http.get("http://localhost:4000/admin/home");
+        {
+          this.setToken("reqTkn", res.reqToken)
+          console.log("Request Token: ", this.getToken("reqTkn"))
+          this.setToken("refTkn", res.refreshToken)
+          console.log("Refresh Token: ", this.getToken("refTkn"))
+          this.router.navigate(['/admin/home'])
+        }
+      })
     }
-  }
-
+    
+    isTokenExpired() {
+      let tkn = localStorage.getItem("reqTkn")
+      if (!tkn) return true
+      
+      let decode = jwtDecode(tkn)
+      if (!decode.exp) return true
+      let expiration_time = decode.exp * 1000
+      let timeNow = new Date().getTime()
+      
+      //console.log("Value of experation and current time: ", expiration_time, timeNow)
+      
+      return expiration_time > timeNow
+    }
+    
+    
+    getData(): Observable<any> {
+      
+      if (!this.isTokenExpired()) {
+        
+        return this.refToken().pipe(
+          switchMap(() => {
+            
+            console.log("Sending the get request after token refresh");
+            return this.http.get("http://localhost:4000/admin/home");
+          }),
+          catchError(error => {
+            
+            console.error('Error refreshing token', error);
+            return of(false); // Handle error appropriately
+          })
+        );
+      } 
+      else {
+        
+        console.log("Sending the get request without token refresh");
+        return this.http.get("http://localhost:4000/admin/home");
+      }
+    }
+    
   refToken(): Observable<any> {
-
+      
     const tkn = {
-
+        
       token: localStorage.getItem("refTkn")
     };
-
+      
     return this.http.post("http://localhost:3000/token", tkn)
     .pipe(tap((res: any) => {
-
-        if (res.status) {
-
-          console.log("Refreshed token: ", res);
-          this.setToken("reqTkn", res.reqToken);
-        }
+        
+      if (res.status) {
+          
+        console.log("Refreshed token: ", res);
+        this.setToken("reqTkn", res.reqToken);
+      }
       }),
       catchError(error => {
-
+        
         console.error('Error refreshing token', error);
-        return of(null); // Handle error appropriately
+        return of(false); // Handle error appropriately
       })
     );
   }
